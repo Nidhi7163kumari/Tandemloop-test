3e66491a3319e33bcea6c60fc7c189d1059a7719
Nidhi <nidhikumari7163@gmail.com>
2025-07-01 15:48:44 +0530

    Operation Problem-1 solution

---
+++ Problem-1.js
@@ -0,0 +1,28 @@
+class Calculator {
+    calculate(a, b, operation) {
+        if (operation === 'add'){
+            return a+b;
+        } else if (operation === 'subtract') {
+            return a-b;
+        } else if (operation === 'multiply'){
+            return a*b;
+        }else if (operation === 'divide'){
+            if (b === 0) {
+            return "cannot divide by zero";
+            }else {
+                return a/b;
+            }
+                
+            }else {
+                 return "invalid operation";
+        }
+
+    }
+}
+const Calc =new Calculator();
+console.log(Calc.calculate(10,5, 'add')); // output: 15
+console.log(Calc.calculate(10,5, 'subtract')); // output: 5
+console.log(Calc.calculate(10,5, 'multiply')); // output: 50
+console.log(Calc.calculate(10,5, 'divide')); // output: 2
+console.log(Calc.calculate(10,0, 'divide'));
+console.log(Calc.calculate(10,2, 'pow')) // output: cannot divide by zero
